77de2ca7d70ba1791811592eff6e77eb6b9bec25
Stesha Chen <xuchen@t2mobile.com>
2017-07-24 09:45:39 +0800

    cat-click by knockout js

---
+++ cat-clicker-knockout/js/app.js
@@ -0,0 +1,19 @@
+var Cat = function() {
+  this.catName = ko.observable('Tobby');
+  this.clickNum = ko.observable(0);
+  this.catSrc = ko.observable('img/cat1.jpeg');
+  this.nickNames = ko.observableArray([
+    {nickName: 'kitty'},
+    {nickName: 'honey'},
+    {nickName: 'sweety'}
+  ]);
+}
+var ViewModel = function() {
+  this.currentCat = ko.observable(new Cat());
+
+  this.incrementClick = function() {
+    this.currentCat().clickNum(this.currentCat().clickNum() + 1);
+  };
+};
+
+ko.applyBindings(new ViewModel());
